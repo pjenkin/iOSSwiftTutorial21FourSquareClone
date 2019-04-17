157ec4f7cc9c95a4dc4d503393c1169e2463c123
Peter Jenkin <peternjenkin@gmail.com>
2019-04-17 16:24:57 +0100

    21-218 Annotations added to map

--- Parse Foursquare Clone/locationVC.swift
+++ Parse Foursquare Clone/locationVC.swift
@@ -39,9 +39,31 @@ class locationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()                     // immediately start locating
 
+        // set map up to recognize long-presses to make annotation of location
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(locationVC.pressGesture(gestureRecognizer: )))
+        recognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(recognizer)
         
     }
 
+    @objc func pressGesture(gestureRecognizer: UIGestureRecognizer)     // NB @objc
+    {
+        if gestureRecognizer.state == UIGestureRecognizerState.began
+        {
+            let touches = gestureRecognizer.location(in: self.mapView)
+            //let coordinates = self.mapView.convert(<#T##point: CGPoint##CGPoint#>, toCoordinateFrom: <#T##UIView?#>)
+            let coordinates = self.mapView.convert(touches, toCoordinateFrom: self.mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinates     // use user touches on map
+            annotation.title = "annotation title here"
+            annotation.subtitle = "type of the place"
+            
+            self.mapView.addAnnotation(annotation)
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -62,8 +84,44 @@ class locationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)     // use *location* parameter when updated
+        
+        manager.stopUpdatingLocation()             // location obtained - map can now pan freely
+        
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)      // 'zoom' 0.05 pretty average
         let region = MKCoordinateRegion(center: location, span: span)
         mapView.setRegion(region, animated: true)
     }
+    
+    
+    // NB relatively recent way of adding notations to map - else no subtitles - from StackOverflow somewhere
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // if the annotation concerned is coming from a phone's location, don't annotate in this way (useful in directions-to/from situation)
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let reuseId = "myAnnotation"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        // reuse just the 1 annotation view (good for memory resources)
+        
+        if pinView == nil
+        {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView?.canShowCallout = true
+            pinView?.pinTintColor = UIColor.red
+            
+            
+            let button = UIButton(type: .detailDisclosure)      // .detailDisclosure == UIButton.detailDisclosure
+            pinView?.rightCalloutAccessoryView = button
+        }
+        else    // if pinView already existent (already defined, and reused)
+        {
+            pinView?.annotation = annotation
+        }
+        return pinView
+    }
+
+    
 }
