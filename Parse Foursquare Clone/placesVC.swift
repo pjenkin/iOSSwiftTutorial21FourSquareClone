09bf40f68e39a03feb657894de02c07c235e3087
Peter Jenkin <peternjenkin@gmail.com>
2019-04-17 12:41:12 +0100

    21-212 Logout for user, resetting entry point & removing UserDefaults state record

--- Parse Foursquare Clone/placesVC.swift
+++ Parse Foursquare Clone/placesVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 class placesVC: UIViewController {
 
@@ -31,5 +32,37 @@ class placesVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func logOutBtnClicked(_ sender: Any) {
+        PFUser.logOutInBackground { (error) in
+            if error != nil
+            {
+                let alert = UIAlertController(title:"Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                
+                let okButton = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                
+                alert.addAction(okButton)
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+            else
+            {
+                // wipe UserDefaults state memory of log-in
+                UserDefaults.standard.removeObject(forKey: "userLoggedIn")
+                UserDefaults.standard.synchronize()
+                
+                // reset entry point
+                let signUpController = self.storyboard?.instantiateViewController(withIdentifier: "signUpVC") as! signUpVC
+                let delegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+                
+                delegate.window?.rootViewController = signUpController
+                
+                delegate.rememberLogin()
+            }
+            // pasted in after first alert code written
+            
+            
+            
+        }
+    }
+    
 }
