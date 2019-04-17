775cfa1a918e01c7a8f51dca3bffadef7bff467b
Peter Jenkin <peternjenkin@gmail.com>
2019-04-17 15:55:15 +0100

    21-217 User's (phone's) location used to initialise map

--- Parse Foursquare Clone/locationVC.swift
+++ Parse Foursquare Clone/locationVC.swift
@@ -10,15 +10,18 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class locationVC: UIViewController {
+class locationVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
+
+    var manager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+/*
+        // checking
         let latitude : CLLocationDegrees = 50
         let longitude : CLLocationDegrees = -4
         
@@ -29,6 +32,14 @@ class locationVC: UIViewController {
         let region : MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
         
         mapView.setRegion(region, animated: true)
+ */
+        mapView.delegate = self
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest   // NB battery drain - also km, 100m, 10m, 3km, ....
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()                     // immediately start locating
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,4 +60,10 @@ class locationVC: UIViewController {
     @IBAction func saveBtnClicked(_ sender: Any) {
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)     // use *location* parameter when updated
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)      // 'zoom' 0.05 pretty average
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 }
